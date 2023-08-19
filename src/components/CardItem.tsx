830a58ed9329b86ce15b5c69487283a2a86d42ad
Jiho Song <jihojiho2003@gmail.com>
2023-08-19 14:05:57 +0900

    add `CardItem.tsx`

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,10 +1,45 @@
-export const CardItem: React.FC = () => {
+import Image from 'next/image';
+
+export type CardItemProps = {
+  card: string;
+  name: string;
+  type: string;
+  address: string;
+  grade: string;
+};
+
+export const CardItem: React.FC<CardItemProps> = ({
+  card,
+  name,
+  type,
+  address,
+  grade,
+}) => {
   return (
     <div className="flex w-full gap-2 p-1 overflow-hidden rounded-md bg-zinc-900">
-      <div className="w-[72px] h-[72px] bg-black rounded-sm aspect-square relative z-0"></div>
-      <div className="flex flex-col">
-        <span>Name</span>
+      <div className="w-[72px] h-[72px] bg-black rounded-sm aspect-square relative z-0">
+        <Image src={card} alt="이미지" />
       </div>
+      <div className="flex flex-col w-full justify-evenly">
+        <span
+          className="text-white text-2xl font-bold"
+          style={{ fontFamily: 'koverwatch' }}
+        >
+          {name}
+        </span>
+        <div className="flex items-baseline">
+          <span
+            className="w-fit text-l font-bold text-white mr-2"
+            style={{ fontFamily: 'koverwatch' }}
+          >
+            {type}
+          </span>
+          <span className="w-full text-l font-bold text-gray-300 ">
+            {address}
+          </span>
+        </div>
+      </div>
+      <div className="w-[72px] h-[72px]">GRADE</div>
     </div>
   );
 };
