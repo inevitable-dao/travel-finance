cb7f729ba4504e9c3df5cba1c2ab8648732b6e9b
Jiho Song <jihojiho2003@gmail.com>
2023-08-20 02:47:40 +0900

    cardopen

--- src/cards/CardOpenPage.tsx
+++ src/cards/CardOpenPage.tsx
@@ -1,9 +1,28 @@
+import axios from 'axios';
 import { NextPage } from 'next';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Button } from '@/components/Button';
+import { CardItem } from '@/components/CardItem';
 import { PageTitle } from '@/components/PageTitle';
+
+type CommonResponse<T> = {
+  statusCode: number;
+  timeStamp: string;
+  path: string;
+  result: T;
+};
+
+type CardItem = {
+  id: string;
+  type: string;
+  name: string;
+  description: string;
+  address: string;
+  estimatedHours: number;
+  rank: number;
+};
 
 enum Stage {
   COVER = 'COVER',
@@ -12,6 +31,25 @@ enum Stage {
 
 const CardOpenPage: NextPage = () => {
   const [stage, setStage] = useState<Stage>(Stage.COVER);
+
+  const [cards, setCards] = useState<CardItem[]>([]);
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await axios.post<CommonResponse<{ cards: CardItem[] }>>(
+        'https://stevejkang.jp.ngrok.io/card-packages/1',
+        undefined,
+        {
+          headers: {
+            'X-Inevitable-Auth-Key': localStorage.getItem('access_token'),
+          },
+        },
+      );
+
+      setCards(res.data.result.cards);
+    };
+
+    fetch();
+  }, []);
 
   return (
     <div className="flex flex-col items-center mt-[64px]">
@@ -33,9 +71,25 @@ const CardOpenPage: NextPage = () => {
 
       {stage === Stage.RESULT && (
         <div>
-          <h2 className="text-white">Results</h2>
-
-          <div className="flex justify-center w-full gap-2 mt-2">
+          <h2
+            className="text-3xl font-medium text-white"
+            style={{ fontFamily: 'koverwatch' }}
+          >
+            Results
+          </h2>
+          {cards.map((card) => (
+            // eslint-disable-next-line react/jsx-key
+            <div className="my-2">
+              <CardItem
+                card={''}
+                name={card.name}
+                type={card.type}
+                address={card.address}
+                rank={card.rank}
+              />
+            </div>
+          ))}
+          <div className="flex justify-center w-full gap-2 mt-4">
             <Link href="/inventory">
               <Button>Inventory</Button>
             </Link>
