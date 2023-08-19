2a11ee2343611f3792bdf49dce9caf1336162dcc
Junho Yeo <i@junho.io>
2023-08-19 10:23:46 +0900

    Add move to top in pagetitle

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,14 +1,21 @@
+import { useCallback } from 'react';
+
 export const PageTitle: React.FC<
   React.PropsWithChildren & {
     subtitle?: string;
     description?: string;
   }
 > = ({ children, subtitle, description }) => {
+  const moveToTop = useCallback(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, []);
+
   return (
     <div className="fixed top-0 flex flex-col w-full mt-4 max-w-2xl px-5 mx-auto pointer-events-none z-[60]">
       <h1
-        className="text-3xl font-bold text-white"
+        className="text-3xl font-bold text-white cursor-pointer pointer-events-auto"
         style={{ fontFamily: 'koverwatch' }}
+        onClick={moveToTop}
       >
         {children}
       </h1>
