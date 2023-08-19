e9dd8961c43709da623c00f7c75faaac8f1882e7
Junho Yeo <i@junho.io>
2023-08-20 05:44:46 +0900

    Clean

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,6 @@
-import type { Identifier, XYCoord } from 'dnd-core';
-import update from 'immutability-helper';
 import { useAtom } from 'jotai';
 import { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
 import React from 'react';
-import type { FC } from 'react';
-import { useCallback, useState } from 'react';
-// import type { FC } from 'react'
-import { useRef } from 'react';
-import { DndProvider } from 'react-dnd';
-import { useDrag, useDrop } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import { BottomTabBar } from '@/components/BottomTabBar';
 import { MobileContainer } from '@/components/MobileContainer';
@@ -20,8 +10,6 @@ import { GlobalStyle } from '@/styles/GlobalStyle';
 import '@/styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
   // hide bottom tab bar when current page is `/cards/open`.
   const [tabBarShown] = useAtom(bottomTabNavigatorShown);
 
@@ -32,9 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       {/* @ts-ignore // Type mismatch after upgrading to React 18 */}
       <GlobalStyle />
 
-      <DndProvider backend={HTML5Backend}>
-        <Component {...pageProps} />
-      </DndProvider>
+      <Component {...pageProps} />
 
       <div id="portal" />
 
